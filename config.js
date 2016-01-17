39eb90cca3be3afca553f4c7079771ab43ac5571
Shane Murtagh <murtaghshane3@gmail.com>
2016-01-17 15:00:32 +0000

    Changedatabase connection

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://shane:xru8787@ds045004-a0.mongolab.com:45004,ds045004-a1.mongolab.com:45004/wanderer?replicaSet=rs-ds045004'
+	'database': 'mongodb://shane:xru8787@ds043971.mongolab.com:43971/melmoth'
 }
